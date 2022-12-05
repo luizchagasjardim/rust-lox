ff52e8c6c89ba5ccdf3aa6b520859a6faf67b8f9
Luiz Chagas Jardim <luiz.jardim@velocix.com>
2022-12-05 19:06:45 +0000

    command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate exitcode;
+
+use std::process::exit;
+use std::env::args;
+
+use clap::Parser;
+
+/// Lox interpreter written in Rust
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path of the file to be executed
+    file_path: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    match args.file_path {
+        None => run_prompt(),
+        Some(file) => run_file(file),
+    };
+}
+
+fn run_prompt() {
+    unimplemented!();
+}
+
+fn run_file(path: String) {
+    unimplemented!("path={}", path);
 }
