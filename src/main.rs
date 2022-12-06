8e6322536faf22bf8c0dee8e54d5329d1e42ea28
Luiz Chagas Jardim <luiz.jardim@velocix.com>
2022-12-06 00:07:09 +0000

    cargo lock

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+#![feature(let_else)]
+
 extern crate exitcode;
 
+use std::io::{Error as IoError, stdin};
 use std::process::exit;
-use std::env::args;
 
 use clap::Parser;
 
@@ -16,15 +18,37 @@ struct Args {
 fn main() {
     let args = Args::parse();
     match args.file_path {
-        None => run_prompt(),
+        None => repl(),
         Some(file) => run_file(file),
     };
 }
 
-fn run_prompt() {
-    unimplemented!();
+fn repl() {
+    loop {
+        let Ok(input) = read() else {
+            exit(exitcode::USAGE);
+        };
+        let Ok(result) = eval(&input) else {
+            exit(exitcode::USAGE);
+        };
+        println!("{}", result);
+    }
 }
 
 fn run_file(path: String) {
     unimplemented!("path={}", path);
+}
+
+fn read() -> Result<String, IoError> {
+    let mut input = String::new();
+    if let Err(e) = stdin().read_line(&mut input) {
+        return Err(e);
+    }
+    Ok(input)
+}
+
+struct EvalError;
+
+fn eval(input: &String) -> Result<String, EvalError> {
+    Ok(input.clone()) //TODO
 }
