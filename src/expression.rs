30e7f08964c589e61566968ce256ecbbe0a3041a
Luiz Chagas Jardim <luiz.jardim@velocix.com>
2022-12-12 19:53:02 +0000

    expression structs

---
+++ src/expression.rs
@@ -0,0 +1,51 @@
+// expression     → literal
+// | unary
+// | binary
+// | grouping ;
+//
+// literal        → NUMBER | STRING | "true" | "false" | "nil" ;
+// grouping       → "(" expression ")" ;
+// unary          → ( "-" | "!" ) expression ;
+// binary         → expression operator expression ;
+// operator       → "==" | "!=" | "<" | "<=" | ">" | ">="
+// | "+"  | "-"  | "*" | "/" ;
+
+enum Expression {
+    Literal(Literal),
+    Unary(Box<Unary>),
+    Binary(Box<Binary>),
+    Grouping(Box<Grouping>),
+}
+
+enum Literal {
+    Number(f64),
+    String(String),
+    True,
+    False,
+    Nil,
+}
+
+struct Grouping(Expression);
+
+struct Unary(Expression);
+
+struct Binary {
+    left: Expression,
+    operator: Operator,
+    right: Expression,
+}
+
+enum Operator {
+    Equality,
+    Different,
+    Less,
+    EqualOrLess,
+    Greater,
+    EqualOrGreater,
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division,
+}
+// operator       → "==" | "!=" | "<" | "<=" | ">" | ">="
+// | "+"  | "-"  | "*" | "/" ;
