fd37c92eaefeefc20847f33860ffec14ecbd64f9
Luiz Chagas Jardim <luiz.jardim@velocix.com>
2022-12-12 19:52:44 +0000

    parser struct

---
+++ src/parser.rs
@@ -0,0 +1,13 @@
+use crate::token::Token;
+
+pub struct Parser {
+    tokens: Vec<Token>,
+}
+
+impl Parser {
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Self {
+            tokens
+        }
+    }
+}
