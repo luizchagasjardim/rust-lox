7bf5cfd846f79fb1d293a96dcfe7f010fdd7cdbc
sousajo-cc <jorge.pinto.sousa@protonmail.ch>
2022-12-26 12:47:00 +0000

    token: add unit tests

--- src/token.rs
+++ src/token.rs
@@ -92,3 +92,103 @@ pub struct Token {
     pub token_type: TokenType,
     pub start: usize,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn left_paren_length() {
+        assert_eq!(TokenType::LeftParen.length(), 1,);
+    }
+    #[test]
+    fn right_paren_length() {
+        assert_eq!(TokenType::RightParen.length(), 1,);
+    }
+    #[test]
+    fn left_brace_length() {
+        assert_eq!(TokenType::LeftBrace.length(), 1,);
+    }
+    #[test]
+    fn right_brace_length() {
+        assert_eq!(TokenType::RightBrace.length(), 1,);
+    }
+    #[test]
+    fn comma_length() {
+        assert_eq!(TokenType::Comma.length(), 1,);
+    }
+    #[test]
+    fn dot_length() {
+        assert_eq!(TokenType::Dot.length(), 1,);
+    }
+    #[test]
+    fn minus_length() {
+        assert_eq!(TokenType::Minus.length(), 1,);
+    }
+
+    #[test]
+    fn plus_length() {
+        assert_eq!(TokenType::Plus.length(), 1,);
+    }
+
+    #[test]
+    fn semicolon_length() {
+        assert_eq!(TokenType::Semicolon.length(), 1,);
+    }
+
+    #[test]
+    fn slash_length() {
+        assert_eq!(TokenType::Slash.length(), 1,);
+    }
+    #[test]
+    fn star_length() {
+        assert_eq!(TokenType::Star.length(), 1,);
+    }
+    #[test]
+    fn bang_length() {
+        assert_eq!(TokenType::Bang.length(), 1,);
+    }
+
+    #[test]
+    fn bang_equal_length() {
+        assert_eq!(TokenType::BangEqual.length(), 2,);
+    }
+    #[test]
+    fn equal_length() {
+        assert_eq!(TokenType::Equal.length(), 1,);
+    }
+    #[test]
+    fn equal_equal_length() {
+        assert_eq!(TokenType::EqualEqual.length(), 2,);
+    }
+
+    #[test]
+    fn greater_length() {
+        assert_eq!(TokenType::Greater.length(), 1,);
+    }
+    #[test]
+    fn greater_equal_length() {
+        assert_eq!(TokenType::GreaterEqual.length(), 2,);
+    }
+    #[test]
+    fn less_length() {
+        assert_eq!(TokenType::Less.length(), 1,);
+    }
+    #[test]
+    fn less_equal_length() {
+        assert_eq!(TokenType::LessEqual.length(), 2,);
+    }
+
+    #[test]
+    fn identifier_length() {
+        assert_eq!(
+            TokenType::Identifier("baseado".parse().unwrap()).length(),
+            7,
+        );
+    }
+
+    #[test]
+    fn eof_length() {
+        assert_eq!(TokenType::EOF.length(), 0,);
+    }
+}
