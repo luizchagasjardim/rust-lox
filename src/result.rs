b2f3c0dec6ee6ba873ee1516b26ede18c99bf03d
Luiz Chagas Jardim <luiz.jardim@velocix.com>
2022-12-26 12:15:31 +0000

    result test

--- src/result.rs
+++ src/result.rs
@@ -38,3 +38,177 @@ impl From<IoError> for Error {
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Debug)]
+    struct MockOsError;
+    impl std::fmt::Display for MockOsError {
+        fn fmt(&self, _: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            unimplemented!()
+        }
+    }
+    impl std::error::Error for MockOsError {}
+    unsafe impl Sync for MockOsError {}
+    unsafe impl Send for MockOsError {}
+
+    #[test]
+    fn error_from_io_error() {
+        let io_error = IoError::new(std::io::ErrorKind::NotFound, MockOsError {});
+        let error: Error = io_error.into();
+        assert!(matches!(error, Error::IoError(_)));
+    }
+
+    mod exit_code {
+        use super::*;
+
+        #[test]
+        fn expected_expression() {
+            let error = Error::ExpectedExpression { position: 0 };
+            assert_eq!(error.exit_code(), exitcode::USAGE);
+        }
+
+        #[test]
+        fn io_error() {
+            let io_error = IoError::new(std::io::ErrorKind::NotFound, MockOsError {});
+            let error: Error = io_error.into();
+            assert_eq!(error.exit_code(), exitcode::IOERR);
+        }
+
+        #[test]
+        fn keyboard_interrupt() {
+            let error = Error::KeyboardInterrupt;
+            assert_eq!(error.exit_code(), exitcode::OK);
+        }
+
+        #[test]
+        fn out_of_line_numbers() {
+            let error = Error::OutOfLineNumbers;
+            assert_eq!(error.exit_code(), exitcode::SOFTWARE);
+        }
+
+        #[test]
+        fn unexpected_character() {
+            let error = Error::UnexpectedCharacter {
+                character: 'a',
+                position: 0,
+            };
+            assert_eq!(error.exit_code(), exitcode::USAGE);
+        }
+
+        #[test]
+        fn unexpected_eof() {
+            let error = Error::UnexpectedEof;
+            assert_eq!(error.exit_code(), exitcode::USAGE);
+        }
+
+        #[test]
+        fn unterminated_number() {
+            let error = Error::UnterminatedNumber {
+                string: "".to_string(),
+                position: 0,
+            };
+            assert_eq!(error.exit_code(), exitcode::USAGE);
+        }
+
+        #[test]
+        fn unterminated_string() {
+            let error = Error::UnterminatedString {
+                string: "".to_string(),
+                position: 0,
+            };
+            assert_eq!(error.exit_code(), exitcode::USAGE);
+        }
+
+        #[test]
+        fn unmatched_parenthesis() {
+            let error = Error::UnmatchedParenthesis { position: 0 };
+            assert_eq!(error.exit_code(), exitcode::USAGE);
+        }
+    }
+
+    mod debug {
+        use super::*;
+
+        #[test]
+        fn expected_expression() {
+            let error = Error::ExpectedExpression { position: 0 };
+            assert_eq!(format!("{:?}", error), "ExpectedExpression { position: 0 }");
+        }
+
+        #[test]
+        fn io_error() {
+            let io_error = IoError::new(std::io::ErrorKind::NotFound, MockOsError {});
+            let error: Error = io_error.into();
+            assert_eq!(
+                format!("{:?}", error),
+                "IoError(Custom { kind: NotFound, error: MockOsError })"
+            );
+        }
+
+        #[test]
+        fn keyboard_interrupt() {
+            let error = Error::KeyboardInterrupt;
+            assert_eq!(format!("{:?}", error), "KeyboardInterrupt");
+        }
+
+        #[test]
+        fn out_of_line_numbers() {
+            let error = Error::OutOfLineNumbers;
+            assert_eq!(format!("{:?}", error), "OutOfLineNumbers");
+        }
+
+        #[test]
+        fn unexpected_character() {
+            let error = Error::UnexpectedCharacter {
+                character: 'a',
+                position: 0,
+            };
+            assert_eq!(
+                format!("{:?}", error),
+                "UnexpectedCharacter { character: 'a', position: 0 }"
+            );
+        }
+
+        #[test]
+        fn unexpected_eof() {
+            let error = Error::UnexpectedEof;
+            assert_eq!(format!("{:?}", error), "UnexpectedEof");
+        }
+
+        #[test]
+        fn unterminated_number() {
+            let error = Error::UnterminatedNumber {
+                string: "".to_string(),
+                position: 0,
+            };
+            assert_eq!(
+                format!("{:?}", error),
+                "UnterminatedNumber { string: \"\", position: 0 }"
+            );
+        }
+
+        #[test]
+        fn unterminated_string() {
+            let error = Error::UnterminatedString {
+                string: "".to_string(),
+                position: 0,
+            };
+            assert_eq!(
+                format!("{:?}", error),
+                "UnterminatedString { string: \"\", position: 0 }"
+            );
+        }
+
+        #[test]
+        fn unmatched_parenthesis() {
+            let error = Error::UnmatchedParenthesis { position: 0 };
+            assert_eq!(
+                format!("{:?}", error),
+                "UnmatchedParenthesis { position: 0 }"
+            );
+        }
+    }
+}
