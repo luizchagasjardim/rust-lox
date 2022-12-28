08062cccc3487588dcf1d8435815a5e8f605f266
sousajo-cc <jorge.pinto.sousa@protonmail.ch>
2022-12-28 13:43:27 +0000

    full line coverage on evalute

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -58,3 +58,149 @@ impl Evaluate for Literal {
         Ok(object)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn evaluate_literal() {
+        let test_values = vec![
+            (Literal::Number(123.4), Object::Number(123.4)),
+            (
+                Literal::String("hello".to_string()),
+                Object::String("hello".to_string()),
+            ),
+            (Literal::True, Object::Boolean(true)),
+            (Literal::False, Object::Boolean(false)),
+            (Literal::Nil, Object::Nil),
+        ];
+
+        for (lit, obj) in test_values {
+            let result = lit.evaluate();
+            assert_eq!(result, Ok(obj));
+        }
+    }
+
+    #[test]
+    fn evaluate_unary() {
+        let test_values = vec![
+            (
+                Expression::Unary {
+                    operator: UnaryOperator::Negation,
+                    expression: Box::new(Expression::Literal(Literal::True)),
+                },
+                Object::Boolean(false),
+            ),
+            (
+                Expression::Unary {
+                    operator: UnaryOperator::Negation,
+                    expression: Box::new(Expression::Literal(Literal::False)),
+                },
+                Object::Boolean(true),
+            ),
+            (
+                Expression::Unary {
+                    operator: UnaryOperator::Minus,
+                    expression: Box::new(Expression::Literal(Literal::Number(123.4))),
+                },
+                Object::Number(-123.4),
+            ),
+        ];
+
+        for (input, obj) in test_values {
+            let result = input.evaluate();
+            assert_eq!(result, Ok(obj));
+        }
+    }
+
+    #[test]
+    fn evaluate_binary() {
+        let test_values = vec![
+            (
+                Expression::Binary {
+                    left: Box::new(Expression::Literal(Literal::Number(1.0))),
+                    operator: BinaryOperator::Addition,
+                    right: Box::new(Expression::Literal(Literal::Number(2.0))),
+                },
+                Object::Number(3.0),
+            ),
+            (
+                Expression::Binary {
+                    left: Box::new(Expression::Literal(Literal::Number(2.0))),
+                    operator: BinaryOperator::Subtraction,
+                    right: Box::new(Expression::Literal(Literal::Number(1.0))),
+                },
+                Object::Number(1.0),
+            ),
+            (
+                Expression::Binary {
+                    left: Box::new(Expression::Literal(Literal::Number(2.0))),
+                    operator: BinaryOperator::Multiplication,
+                    right: Box::new(Expression::Literal(Literal::Number(3.0))),
+                },
+                Object::Number(6.0),
+            ),
+            (
+                Expression::Binary {
+                    left: Box::new(Expression::Literal(Literal::Number(4.0))),
+                    operator: BinaryOperator::Division,
+                    right: Box::new(Expression::Literal(Literal::Number(2.0))),
+                },
+                Object::Number(2.0),
+            ),
+            (
+                Expression::Binary {
+                    left: Box::new(Expression::Literal(Literal::Number(4.0))),
+                    operator: BinaryOperator::Greater,
+                    right: Box::new(Expression::Literal(Literal::Number(2.0))),
+                },
+                Object::Boolean(true),
+            ),
+            (
+                Expression::Binary {
+                    left: Box::new(Expression::Literal(Literal::Number(1.0))),
+                    operator: BinaryOperator::Less,
+                    right: Box::new(Expression::Literal(Literal::Number(2.0))),
+                },
+                Object::Boolean(true),
+            ),
+            (
+                Expression::Binary {
+                    left: Box::new(Expression::Literal(Literal::Number(1.0))),
+                    operator: BinaryOperator::EqualOrLess,
+                    right: Box::new(Expression::Literal(Literal::Number(2.0))),
+                },
+                Object::Boolean(true),
+            ),
+            (
+                Expression::Binary {
+                    left: Box::new(Expression::Literal(Literal::Number(2.0))),
+                    operator: BinaryOperator::EqualOrGreater,
+                    right: Box::new(Expression::Literal(Literal::Number(2.0))),
+                },
+                Object::Boolean(true),
+            ),
+            (
+                Expression::Binary {
+                    left: Box::new(Expression::Literal(Literal::Number(2.0))),
+                    operator: BinaryOperator::Equality,
+                    right: Box::new(Expression::Literal(Literal::Number(2.0))),
+                },
+                Object::Boolean(true),
+            ),
+            (
+                Expression::Binary {
+                    left: Box::new(Expression::Literal(Literal::Number(3.0))),
+                    operator: BinaryOperator::Different,
+                    right: Box::new(Expression::Literal(Literal::Number(2.0))),
+                },
+                Object::Boolean(true),
+            ),
+        ];
+        for (input, obj) in test_values {
+            let result = input.evaluate();
+            assert_eq!(result, Ok(obj));
+        }
+    }
+}
