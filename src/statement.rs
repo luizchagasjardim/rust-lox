02f8fac344b9d3121538ccc876dbdf5677f3675d
Luiz Chagas Jardim <luiz.jardim@velocix.com>
2023-02-04 10:13:26 +0000

    if statement

--- src/statement.rs
+++ src/statement.rs
@@ -3,6 +3,11 @@ use crate::expression::Expression;
 #[derive(PartialEq, Debug)]
 pub enum Statement {
     Expression(Expression),
+    If {
+        condition: Expression,
+        then_statement: Box<Statement>,
+        else_statement: Box<Statement>,
+    },
     Print(Expression),
     VariableDeclaration {
         identifier: String,
