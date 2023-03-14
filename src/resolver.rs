2b3795d5dd555153051f544486e0c638b019ef4f
Luiz Chagas Jardim <luiz.jardim@velocix.com>
2023-03-14 13:54:23 +0000

    declare and define

--- src/resolver.rs
+++ src/resolver.rs
@@ -3,9 +3,14 @@ use crate::interpreter::Interpreter;
 use crate::statement::Statement;
 use std::collections::HashMap;
 
+enum VariableStatus {
+    Declared,
+    Defined,
+}
+
 struct Resolver {
     interpreter: Interpreter,
-    scopes: Vec<HashMap<String, bool>>,
+    scopes: Vec<HashMap<String, VariableStatus>>,
 }
 
 impl Resolver {
@@ -22,7 +27,16 @@ impl Resolver {
             Statement::If { .. } => todo!(),
             Statement::Print(_) => todo!(),
             Statement::Return(_) => todo!(),
-            Statement::VariableDeclaration { .. } => todo!(),
+            Statement::VariableDeclaration {
+                identifier,
+                expression,
+            } => {
+                self.declare(identifier.clone());
+                if let Some(initializer) = expression {
+                    self.resolve_expression(initializer);
+                }
+                self.define(identifier);
+            }
             Statement::FunctionDeclaration(_) => todo!(),
             Statement::While { .. } => todo!(),
             Statement::Block(statements) => {
@@ -53,5 +67,19 @@ impl Resolver {
 
     fn end_scope(&mut self) {
         self.scopes.pop();
+    }
+
+    fn declare(&mut self, identifier: String) {
+        let Some(scope) = self.scopes.last_mut() else {
+            return;
+        };
+        scope.insert(identifier, VariableStatus::Declared);
+    }
+
+    fn define(&mut self, identifier: String) {
+        let Some(scope) = self.scopes.last_mut() else {
+            return;
+        };
+        scope.insert(identifier, VariableStatus::Defined);
     }
 }
